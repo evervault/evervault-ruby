72e2cf1cd16e1c8f69fc794eb488a5f104cca701
Jonny <jonathanomahony@gmail.com>
2020-10-23 12:51:01 +0100

    Add our own release script

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+echo "Setting up gem credentials..."
+set +x
+mkdir -p ~/.gem
+
+cat << EOF > ~/.gem/credentials
+---
+:github: Bearer ${GITHUB_TOKEN}
+:rubygems_api_key: ${RUBYGEMS_API_KEY}
+EOF
+
+chmod 0600 ~/.gem/credentials
+set -x
+
+echo "Installing dependencies..."
+bundle install
+
+echo "Running gem release task..."
+gem bump -k
