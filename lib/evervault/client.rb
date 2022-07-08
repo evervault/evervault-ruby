2ca2e4cbdfffe0bded6010190c0935228f395ae4
DeirdreCleary <deirdre@evervault.com>
2022-07-08 13:55:52 +0100

    Correctly updating default curve

--- lib/evervault/client.rb
+++ lib/evervault/client.rb
@@ -15,7 +15,7 @@ module Evervault
       relay_url: "https://relay.evervault.com:8443",
       ca_host: "https://ca.evervault.com",
       request_timeout: 30,
-      curve: 'secp256r1'
+      curve: 'prime256v1'
     )
       @request = Evervault::Http::Request.new(timeout: request_timeout, api_key: api_key)
       @intercept = Evervault::Http::RequestIntercept.new(
