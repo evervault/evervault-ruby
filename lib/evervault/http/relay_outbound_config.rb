00ad8f9b5d6817f0305732e065b8586ca0c83659
Jeremy Wagemans <jeremy@evervault.com>
2022-12-07 09:42:55 +0000

    improvements

--- lib/evervault/http/relay_outbound_config.rb
+++ lib/evervault/http/relay_outbound_config.rb
@@ -1,17 +1,18 @@
 class RelayOutboundConfig
 
     DEFAULT_POLL_INTERVAL = 5
+    RELAY_OUTBOUND_CONFIG_API_ENDPOINT = "/v2/relay-outbound"
 
     @@destination_domains_cache = nil
     @@timer = nil
 
     def initialize(request_handler)
         @request_handler = request_handler
-        if @@destination_domains_cache.nil?
-            self.get_relay_outbound_config
         if @@timer.nil?
             @@timer = RepeatedTimer.new(DEFAULT_POLL_INTERVAL, self.get_relay_outbound_config)
         end
+        if @@destination_domains_cache.nil?
+            self.get_relay_outbound_config
     end
 
     def get_destination_domains
@@ -27,7 +28,11 @@ class RelayOutboundConfig
         @@destination_domains_cache = nil
     end
 
-    def get_relay_outbound_config
-        @request_handler.get("/v2/relay-outbound")
+    private def get_relay_outbound_config
+        @request_handler.get(RELAY_OUTBOUND_CONFIG_API_ENDPOINT)
+        poll_interval = "5"
+        unless @@timer.nil? || poll_interval.nil?
+            @@timer.update_interval(poll_interval)
+        @@destination_domains_cache = response["destinationDomains"].values.map(&:destinationDomain)
     end
 end
