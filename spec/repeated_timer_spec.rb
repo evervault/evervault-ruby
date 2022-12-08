37b432595ed14f58a55ab2d66730ec5f0c4c0256
Jeremy Wagemans <jeremy@evervault.com>
2022-12-08 13:33:09 +0000

    Attempt at fixing flaky tests

--- spec/repeated_timer_spec.rb
+++ spec/repeated_timer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Evervault do
     describe "initialize" do
       it "should start the timer and invoke the function at regular interval" do
         counter = 0
-        timer = Evervault::Threading::RepeatedTimer.new(0.1, -> { counter += 1 })
+        timer = Evervault::Threading::RepeatedTimer.new(0.2, -> { counter += 1 })
         expect(timer.running?).to be(true)
-        sleep 0.25
+        sleep 0.50
         expect(counter).to be(2)
         timer.stop
       end
@@ -15,7 +15,7 @@ RSpec.describe Evervault do
       it "should silently ignore exceptions thrown by the invoked function" do
         timer = Evervault::Threading::RepeatedTimer.new(0.1, -> { raise "error" })
         expect(timer.running?).to be(true)
-        sleep 0.15
+        sleep 0.20
         timer.stop
       end
     end
@@ -23,21 +23,21 @@ RSpec.describe Evervault do
     describe "start" do
       it "should start a repeated timer if the timer is not already running" do
         counter = 0
-        timer = Evervault::Threading::RepeatedTimer.new(0.1, -> { counter += 1 })
+        timer = Evervault::Threading::RepeatedTimer.new(0.2, -> { counter += 1 })
         timer.stop
         timer.start
         expect(timer.running?).to be(true)
-        sleep 0.25
+        sleep 0.50
         expect(counter).to be(2)
         timer.stop
       end
 
       it "should not start a repeated timer if the timer is already running" do
         counter = 0
-        timer = Evervault::Threading::RepeatedTimer.new(0.1, -> { counter += 1 })
+        timer = Evervault::Threading::RepeatedTimer.new(0.2, -> { counter += 1 })
         timer.start
         expect(timer.running?).to be(true)
-        sleep 0.25
+        sleep 0.50
         expect(counter).to be(2)
         timer.stop
       end
@@ -74,12 +74,11 @@ RSpec.describe Evervault do
     describe "stop" do
       it "should stop the timer" do
         counter = 0
-        timer = Evervault::Threading::RepeatedTimer.new(0.1, -> { counter += 1 })
-        sleep 0.25
+        timer = Evervault::Threading::RepeatedTimer.new(0.2, -> { counter += 1 })
         timer.stop
         expect(timer.running?).to be(false)
-        sleep 0.25
-        expect(counter).to be(2)
+        sleep 0.40
+        expect(counter).to be(0)
       end
     end
   end
