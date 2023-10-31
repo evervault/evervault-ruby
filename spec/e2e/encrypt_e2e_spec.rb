0cecc92de21549a7b5d5f349a1b9eef84c0a4957
DeirdreCleary <deirdre@evervault.com>
2023-10-31 10:57:12 +0000

    Adding tests

--- spec/e2e/encrypt_e2e_spec.rb
+++ spec/e2e/encrypt_e2e_spec.rb
@@ -13,12 +13,38 @@ RSpec.describe Evervault do
       decryptResult = Evervault.decrypt(encryptResult)
       expect(decryptResult).to eq(payload)
     end
+    
+    it "should decrypt a string with a permitting role" do
+      payload = "hello world"
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting a string with a denying role" do
+      payload = "hello world"
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
+    end
 
     it "should encrypt an integer" do
       payload = 1
       encryptResult = Evervault.encrypt(payload)
       decryptResult = Evervault.decrypt(encryptResult)
       expect(decryptResult).to eq(payload)
+    end
+
+    it "should decrypt an integer with a permitting role" do
+      payload = 1
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting an integer with a denying role" do
+      payload = 1
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
     end
 
     it "should encrypt a float" do
@@ -28,11 +54,37 @@ RSpec.describe Evervault do
       expect(decryptResult).to eq(payload)
     end
 
+    it "should decrypt a float with a permitting role" do
+      payload = 1.0
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting a float with a denying role" do
+      payload = 1.0
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
+    end
+
     it "should encrypt a true bool" do
       payload = true
       encryptResult = Evervault.encrypt(payload)
       decryptResult = Evervault.decrypt(encryptResult)
       expect(decryptResult).to eq(payload)
+    end
+
+    it "should decrypt a true bool with a permitting role" do
+      payload = true
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting a true bool with a denying role" do
+      payload = true
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
     end
 
     it "should encrypt a false bool" do
@@ -42,11 +94,37 @@ RSpec.describe Evervault do
       expect(decryptResult).to eq(payload)
     end
 
+    it "should decrypt a false bool with a permitting role" do
+      payload = false
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting a false bool with a denying role" do
+      payload = true
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
+    end
+
     it "should encrypt a hash" do
       payload = {"hello" => "world"}
       encryptResult = Evervault.encrypt(payload)
       decryptResult = Evervault.decrypt(encryptResult)
       expect(decryptResult).to eq(payload)
+    end
+
+    it "should decrypt a hash with a permitting role" do
+      payload = {"hello" => "world"}
+      encryptResult = Evervault.encrypt(payload, "permit-all")
+      decryptResult = Evervault.decrypt(encryptResult)
+      expect(decryptResult).to eq(payload)
+    end
+    
+    it "should error decrypting a hash with a denying role" do
+      payload = {"hello" => "world"}
+      encryptResult = Evervault.encrypt(payload, "deny-all")
+      expect{ Evervault.decrypt(encryptResult) }.to raise_error(Evervault::Errors::EvervaultError)
     end
   end
 end
